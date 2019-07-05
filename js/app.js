d6a3bf2a41124d16e538e8596fddbc81ab7c3fa4
HypeKeyz <leomeque@gmail.com>
2019-07-05 22:42:46 +0200

    added all classes and shuffle method

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
+// Array with classes of every card
+let cardClass = ['apple','banana','strawbery','peach','grape','kiwi','papaya','orange'];
+const gameBoard = document.querySelector('.container'); //grid container for the cards
 
-let gameCards = ['a','b','c'];
-
-function card(event){
-    
+//Cards Click Events
+function cardClickEvents(event){
+     if(event.target.classList.item(1)!=null){         
+         console.log(`Card was clicked: ${event.target.classList.item(1)}`);
+     }
 }
 
-function startGame(){
-    const gameBoard = document.querySelector('.container');
-    
 
-    for(let i=1; i<=16; i++){
-        const div = document.createElement('div');
-        div.setAttribute('class', 'box');
-        gameBoard.insertAdjacentElement('afterbegin',div);
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-    console.log("stargin game");
+
+}
+
+//Starting the game
+function startGame(){
+    gameCards = [...cardClass,...cardClass];
+    shuffle(gameCards);
+    for(let i=0; i<gameCards.length; i++){
+        const card = document.createElement('div');
+        card.setAttribute('class', `box ${gameCards[i]}`);
+        gameBoard.appendChild(card);
+    }
+    gameBoard.addEventListener('click',cardClickEvents);
+    console.log("Game started");
 }
 
 // Starts the game
