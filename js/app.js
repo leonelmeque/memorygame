c9d551b29e4593441555a44cfc0fefada1f30a56
HypeKeyz <leomeque@gmail.com>
2019-07-06 01:58:21 +0200

    added images and more features

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 // Array with classes of every card
-let cardClass = ['apple','banana','strawbery','peach','grape','kiwi','papaya','orange'];
+let cardClass = ['apple','banana','strawberry','avocado','coconut','pineapple','watermelon','orange'];
 const gameBoard = document.querySelector('.container'); //grid container for the cards
+let numOfMoves = 0;
+let firstSelectedCard = null;
+let secondSelectedCard = null;
+let counter = 0;
 
-//Cards Click Events
-function cardClickEvents(event){
+
+// Resets the cards if they are guessed wrong
+function wrongCards(){
+    firstSelectedCard.target.style.pointerEvents = "auto";
+    secondSelectedCard.target.style.pointerEvents = "auto";
+    firstSelectedCard = null;
+    secondSelectedCard = null;
+}
+
+// Check if the selected cards are equal
+function cardChecker(){
+    if (firstSelectedCard.target.classList.item(1)===secondSelectedCard.target.classList.item(1)){
+        firstSelectedCard = null;
+        secondSelectedCard = null;
+      return true;
+    }
+    wrongCards();
+    return false; 
+}
+// Updates the number of moves the player had to do to finish the game
+function updateMoves(){
+    var movesSpan = document.querySelector('#moves');
+    movesSpan.textContent = ++numOfMoves;
+}
+
+// Points to the clicked events that are our cards
+ function cardClickEvents(event){
      if(event.target.classList.item(1)!=null){         
-         console.log(`Card was clicked: ${event.target.classList.item(1)}`);
+         if(!firstSelectedCard){
+            firstSelectedCard = event;
+            firstSelectedCard.target.style.pointerEvents = "none";
+         }else if(!secondSelectedCard){
+            secondSelectedCard = event;
+            secondSelectedCard.target.style.pointerEvents = "none";
+            console.log(cardChecker());
+            updateMoves();          
+         }
+         
      }
 }
 
@@ -25,10 +63,10 @@ function shuffle(array) {
 }
 
 //Starting the game
-function startGame(){
+function startGame() {
     gameCards = [...cardClass,...cardClass];
     shuffle(gameCards);
-    for(let i=0; i<gameCards.length; i++){
+    for(var i=0; i<gameCards.length; i++){
         const card = document.createElement('div');
         card.setAttribute('class', `box ${gameCards[i]}`);
         gameBoard.appendChild(card);
